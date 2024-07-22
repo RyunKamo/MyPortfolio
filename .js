39c5d70337208898a1c256b4aca7d881b9fdae58
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-22 22:17:30 +0900

    英単語の保存機能まで実装完了

--- .js
+++ .js
@@ -1,11 +1,22 @@
 "use strict";
 
+let wordsAndMeanings = [];
+
 // 文字が入力されている時だけボタンが出現する処理
 function appearSaveBtn() {
-  if (document.getElementById("contentFlashCards").value == "") {
+  if (document.getElementById("FlashCardsWords").value == "") {
     document.getElementById("nextBackBtn").style.display = "none";
   } else {
-    console.log("ok");
     document.getElementById("nextBackBtn").style.display = "flex";
+  }
+}
+
+function nextBtn() {
+  // 配列に英単語を入れる
+  if (document.getElementById("FlashCardsWords").value !== "") {
+    wordsAndMeanings.push(document.getElementById("FlashCardsWords").value);
+    document.getElementById("FlashCardsWords").style.display = "none";
+    document.getElementById("FlashCardsMeanigs").style.display = "block";
+    console.log(wordsAndMeanings);
   }
 }
