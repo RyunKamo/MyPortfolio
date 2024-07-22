bd81665457bd2dc3d198137c444cb8030d6b9e1c
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-22 21:24:48 +0900

    文字が入力されている時だけボタンが出現する処理を実装

--- .js
+++ .js
@@ -1,1 +1,11 @@
 "use strict";
+
+// 文字が入力されている時だけボタンが出現する処理
+function appearSaveBtn() {
+  if (document.getElementById("contentFlashCards").value == "") {
+    document.getElementById("nextBackBtn").style.display = "none";
+  } else {
+    console.log("ok");
+    document.getElementById("nextBackBtn").style.display = "flex";
+  }
+}
