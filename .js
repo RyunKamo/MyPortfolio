2144d7a558544874f759c3ec3823909d68f77147
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-23 11:00:17 +0900

    英単語とその意味を複数配列に入れることに成功

--- .js
+++ .js
@@ -4,21 +4,28 @@ let wordsAndMeanings = new Array();
 
 // 文字が入力されている時だけボタンが出現する処理
 function appearSaveBtn() {
-  if (
-    document.getElementById("FlashCardsWords").value !== "" ||
-    document.getElementById("FlashCardsMeanigs").value !== ""
-  ) {
+  if (document.getElementById("FlashCardsWords").value !== "" ||document.getElementById("FlashCardsMeanigs").value !== "") {    
     document.getElementById("nextBackBtn").style.display = "flex";
   } else {
     document.getElementById("nextBackBtn").style.display = "none";
   }
 }
 
+
 function nextBtn() {
-  // 配列に英単語を入れる
-  wordsAndMeanings.push(document.getElementById("words").value);
+  if(document.getElementById("FlashCardsWords").style.display != "none" && document.getElementById("words").value !==""){
+    wordsAndMeanings.push(document.getElementById("words").value);
+    document.getElementById("FlashCardsWords").style.display = "none";
+    document.getElementById("words").value="";
+    document.getElementById("nextBackBtn").style.display = "none";
+    document.getElementById("FlashCardsMeanigs").style.display = "flex";
+    
+  }else if(document.getElementById("FlashCardsMeanigs").style.display = "flex" && document.getElementById("meanings").value !==""){
+    wordsAndMeanings.push(document.getElementById("meanings").value);
+    document.getElementById("FlashCardsMeanigs").style.display = "none";
+    document.getElementById("meanings").value="";
+    document.getElementById("nextBackBtn").style.display = "none";
+    document.getElementById("FlashCardsWords").style.display = "flex";
+  }
   console.log(wordsAndMeanings);
-  document.getElementById("FlashCardsWords").style.display = "none";
-  document.getElementById("FlashCardsMeanigs").style.display = "flex";
-  document.getElementById("nextBackBtn").style.display = "none";
 }
