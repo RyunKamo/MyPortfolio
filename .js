22f81c75ebed0e0c281dc0a75a6eef4c543e1239
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-23 10:37:16 +0900

    保存機能ためのボタンの出現は上手くいった

--- .js
+++ .js
@@ -1,22 +1,24 @@
 "use strict";
 
-let wordsAndMeanings = [];
+let wordsAndMeanings = new Array();
 
 // 文字が入力されている時だけボタンが出現する処理
 function appearSaveBtn() {
-  if (document.getElementById("FlashCardsWords").value == "") {
-    document.getElementById("nextBackBtn").style.display = "none";
-  } else {
+  if (
+    document.getElementById("FlashCardsWords").value !== "" ||
+    document.getElementById("FlashCardsMeanigs").value !== ""
+  ) {
     document.getElementById("nextBackBtn").style.display = "flex";
+  } else {
+    document.getElementById("nextBackBtn").style.display = "none";
   }
 }
 
 function nextBtn() {
   // 配列に英単語を入れる
-  if (document.getElementById("FlashCardsWords").value !== "") {
-    wordsAndMeanings.push(document.getElementById("FlashCardsWords").value);
-    document.getElementById("FlashCardsWords").style.display = "none";
-    document.getElementById("FlashCardsMeanigs").style.display = "block";
-    console.log(wordsAndMeanings);
-  }
+  wordsAndMeanings.push(document.getElementById("words").value);
+  console.log(wordsAndMeanings);
+  document.getElementById("FlashCardsWords").style.display = "none";
+  document.getElementById("FlashCardsMeanigs").style.display = "flex";
+  document.getElementById("nextBackBtn").style.display = "none";
 }
