f7d364ab36779f667792185583185656a81e297d
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-24 12:18:23 +0900

    消去ボタンを作る前の状態

--- .js
+++ .js
@@ -6,9 +6,9 @@ let newWordsAndMeanings = new Array();
 // 文字が入力されている時だけボタンが出現する処理
 function appearSaveBtn() {
   if(document.getElementById("words").value !== "" || document.getElementById("meanings").value !== ""){
-    document.getElementById("nextBackBtn").style.display = "flex";
+    document.getElementById("nextBtnContainer").style.display = "flex";
   }else{
-    document.getElementById("nextBackBtn").style.display = "none";
+    document.getElementById("nextBtnContainer").style.display = "none";
   }
 }
 
@@ -18,14 +18,14 @@ function nextBtn() {
     wordsAndMeanings.push(document.getElementById("words").value);
     document.getElementById("FlashCardsWords").style.display = "none";
     document.getElementById("words").value="";
-    document.getElementById("nextBackBtn").style.display = "none";
+    document.getElementById("nextBtnContainer").style.display = "none";
     document.getElementById("FlashCardsMeanigs").style.display = "flex";
     
   }else if(document.getElementById("FlashCardsMeanigs").style.display = "flex" && document.getElementById("meanings").value !==""){
     wordsAndMeanings.push(document.getElementById("meanings").value);
     document.getElementById("FlashCardsMeanigs").style.display = "none";
     document.getElementById("meanings").value="";
-    document.getElementById("nextBackBtn").style.display = "none";
+    document.getElementById("nextBtnContainer").style.display = "none";
     document.getElementById("FlashCardsWords").style.display = "flex";
 
   }
