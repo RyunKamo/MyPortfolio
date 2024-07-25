341b7433ca10fe56b1cdd43ed9722309d8c82fd1
加茂竜之介 <Kamoryu2001@gmail.com>
2024-07-25 21:24:54 +0900

    単語練習の下準備完了

--- .js
+++ .js
@@ -5,123 +5,134 @@ let newWordsAndMeanings = new Array();
 
 // 文字が入力されている時だけボタンが出現する処理
 function appearSaveBtn() {
-  if(document.getElementById("words").value !== "" || document.getElementById("meanings").value !== ""){
+  if (
+    document.getElementById("words").value !== "" ||
+    document.getElementById("meanings").value !== ""
+  ) {
     document.getElementById("nextBtnContainer").style.display = "flex";
-  }else{
+  } else {
     document.getElementById("nextBtnContainer").style.display = "none";
   }
 }
 
 // 配列に英単語と意味を加える処理
 function nextBtn() {
-  if(document.getElementById("FlashCardsWords").style.display != "none" && document.getElementById("words").value !==""){
+  if (
+    document.getElementById("FlashCardsWords").style.display != "none" &&
+    document.getElementById("words").value !== ""
+  ) {
     wordsAndMeanings.push(document.getElementById("words").value);
     document.getElementById("FlashCardsWords").style.display = "none";
-    document.getElementById("words").value="";
+    document.getElementById("words").value = "";
     document.getElementById("nextBtnContainer").style.display = "none";
     document.getElementById("FlashCardsMeanigs").style.display = "flex";
-    
-      // 一つ前の英単語を消すボタンを出現させる処理
-      if(wordsAndMeanings.length!==0&&wordsAndMeanings.length%2==1){
-        document.getElementById("deleteBtnContainer").style.display="block";
-        document.getElementById("deleteMeaningsBtn").style.display="none";
-        document.getElementById("deleteWordsBtn").style.display="block";
-      }
-  }else if(document.getElementById("FlashCardsMeanigs").style.display = "flex" && document.getElementById("meanings").value !==""){
+
+    // 一つ前の英単語を消すボタンを出現させる処理
+    if (wordsAndMeanings.length !== 0 && wordsAndMeanings.length % 2 == 1) {
+      document.getElementById("deleteBtnContainer").style.display = "block";
+      document.getElementById("deleteMeaningsBtn").style.display = "none";
+      document.getElementById("deleteWordsBtn").style.display = "block";
+    }
+  } else if (
+    (document.getElementById("FlashCardsMeanigs").style.display =
+      "flex" && document.getElementById("meanings").value !== "")
+  ) {
     wordsAndMeanings.push(document.getElementById("meanings").value);
     document.getElementById("FlashCardsMeanigs").style.display = "none";
-    document.getElementById("meanings").value="";
+    document.getElementById("meanings").value = "";
     document.getElementById("nextBtnContainer").style.display = "none";
     document.getElementById("FlashCardsWords").style.display = "flex";
-    
-      // 一つ前の意味を消すボタンを出現させる処理
-      if(wordsAndMeanings.length!==0&&wordsAndMeanings.length%2==0){
-        document.getElementById("deleteBtnContainer").style.display="block";
-        document.getElementById("deleteWordsBtn").style.display="none";
-        document.getElementById("deleteMeaningsBtn").style.display="block";
-      }
 
-  }
-    // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
-    if(wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0){
-      document.getElementById("blackBoardTitle").style.display="none";
-      document.getElementById("useFlashCards").style.display="block";
-    }else{
-      document.getElementById("useFlashCards").style.display="none";
-      document.getElementById("blackBoardTitle").style.display="block";
+    // 一つ前の意味を消すボタンを出現させる処理
+    if (wordsAndMeanings.length !== 0 && wordsAndMeanings.length % 2 == 0) {
+      document.getElementById("deleteBtnContainer").style.display = "block";
+      document.getElementById("deleteWordsBtn").style.display = "none";
+      document.getElementById("deleteMeaningsBtn").style.display = "block";
     }
+  }
+  // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
+  if (wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0) {
+    document.getElementById("blackBoardTitle").style.display = "none";
+    document.getElementById("useFlashCards").style.display = "block";
+  } else {
+    document.getElementById("useFlashCards").style.display = "none";
+    document.getElementById("blackBoardTitle").style.display = "block";
+  }
   // console.log(wordsAndMeanings);
 }
 
 // ✓が付け加えられた際に、（一次元）配列を英単語とその意味ごとに区切りに多次元配列に変更、及び単語帳の種類選択できるよ画面に変更する
-function makeFlashCards(checkbox){
-  if(checkbox.checked){
-    for(let i = 0; wordsAndMeanings.length>i; i++ ){
-      if(newWordsAndMeanings.length===wordsAndMeanings.length/2){
+function makeFlashCards(checkbox) {
+  if (checkbox.checked) {
+    for (let i = 0; wordsAndMeanings.length > i; i++) {
+      if (newWordsAndMeanings.length === wordsAndMeanings.length / 2) {
         break;
-      }else{
-        newWordsAndMeanings.push(wordsAndMeanings.slice(i*2,(i+2)+i*1))
-        document.getElementById("mainPageContent").style.display="none";
-        document.getElementById("selectFlashCardsTypesPage").style.display="flex";
+      } else {
+        newWordsAndMeanings.push(wordsAndMeanings.slice(i * 2, i + 2 + i * 1));
+        document.getElementById("mainPageContent").style.display = "none";
+        document.getElementById("selectFlashCardsTypesPage").style.display =
+          "flex";
       }
     }
-  }else{
-    ;
+  } else {
   }
-  console.log(newWordsAndMeanings)
+  console.log(newWordsAndMeanings);
 }
 
-function deleteBtn(element){
-  console.log(wordsAndMeanings)
-  if(element.id=="deleteWordsBtn"){
+function deleteBtn(element) {
+  console.log(wordsAndMeanings);
+  if (element.id == "deleteWordsBtn") {
     // 配列の最新の要素（＝一番最後の要素）を抽出し、消去する
     wordsAndMeanings.pop();
-    console.log(wordsAndMeanings)
+    console.log(wordsAndMeanings);
     document.getElementById("FlashCardsMeanigs").style.display = "none";
-    document.getElementById("deleteBtnContainer").style.display="none";
-    document.getElementById("deleteMeaningsBtn").style.display="none";
+    document.getElementById("deleteBtnContainer").style.display = "none";
+    document.getElementById("deleteMeaningsBtn").style.display = "none";
     document.getElementById("FlashCardsWords").style.display = "flex";
 
-        // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
-        if(wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0){
-          document.getElementById("blackBoardTitle").style.display="none";
-          document.getElementById("useFlashCards").style.display="block";
-        }else{
-          document.getElementById("useFlashCards").style.display="none";
-          document.getElementById("blackBoardTitle").style.display="block";
-        }
-  }else{
+    // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
+    if (wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0) {
+      document.getElementById("blackBoardTitle").style.display = "none";
+      document.getElementById("useFlashCards").style.display = "block";
+    } else {
+      document.getElementById("useFlashCards").style.display = "none";
+      document.getElementById("blackBoardTitle").style.display = "block";
+    }
+  } else {
     // 配列の最新の要素（＝一番最後の要素）を抽出し、消去する
     wordsAndMeanings.pop();
-    console.log(wordsAndMeanings)
+    console.log(wordsAndMeanings);
     document.getElementById("FlashCardsWords").style.display = "none";
-    document.getElementById("deleteBtnContainer").style.display="none";
-    document.getElementById("deleteWordsBtn").style.display="none";
+    document.getElementById("deleteBtnContainer").style.display = "none";
+    document.getElementById("deleteWordsBtn").style.display = "none";
     document.getElementById("FlashCardsMeanigs").style.display = "flex";
-        
-      // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
-      if(wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0){
-        document.getElementById("blackBoardTitle").style.display="none";
-        document.getElementById("useFlashCards").style.display="block";
-        }else{
-          document.getElementById("useFlashCards").style.display="none";
-          document.getElementById("blackBoardTitle").style.display="block";
-        }
+
+    // 英単語とその意味のペアを一つ以上入力した際に完了ボタンが出現する処理
+    if (wordsAndMeanings.length % 2 == 0 && wordsAndMeanings.length !== 0) {
+      document.getElementById("blackBoardTitle").style.display = "none";
+      document.getElementById("useFlashCards").style.display = "block";
+    } else {
+      document.getElementById("useFlashCards").style.display = "none";
+      document.getElementById("blackBoardTitle").style.display = "block";
+    }
   }
 }
 
 // 単語帳の意味をタイプを選択する画面
-function toPracticePage(checkbox){
-  console.log(checkbox.id)
-  document.getElementById("selectFlashCardsTypesPage").style.display="none";
-  document.getElementById("practicePage").style.display="flex";
-
-  // 
-  // if(checkbox.id==){
-
-  // }else if(){
-
-  // }else{
-
-  // }
+function toPracticePage(checkbox) {
+  document.getElementById("selectFlashCardsTypesPage").style.display = "none";
+  document.getElementById("practicePage").style.display = "flex";
+  
+  if(checkbox.id =="checkboxMemorizeWords"){
+    // 単語のスペルを覚える
+    console.log("単語のスペルを覚える");
+    
+  }else if(checkbox.id =="checkboxMemorizeMeanings"){
+    // 単語の意味を覚える
+    console.log("単語の意味を覚える");
+    
+  }else{
+    // 両方を覚える
+    console.log("両方を覚える");
+  }
 }
